8d968213f63679fe7ae555f95c5f0ee545b480fd
Gloryalejo <65028521+Gloryalejo@users.noreply.github.com>
2024-05-08 19:45:57 -0600

    Add files via upload

---
+++ ToDo/ToDo/ToDoCell.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoCell.swift
+//  ToDo
+//
+//  Created by Alumno on 7/04/24
+//
+
+
+import UIKit
+
+@objc protocol ToDoCellDelegate: class {
+    func checkmarkTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var delegate: ToDoCellDelegate?
+    
+    @IBAction func completeButtonTapped() {
+        delegate?.checkmarkTapped(sender: self)
+    }
+
+}
